0bd09ca90f4ab625f7d186b9007f0ff8d1cf42a1
JodokF <caspar.conradi@posteo.org>
2024-05-24 09:53:21 +0200

    added pid cntrl function -> works ok

--- traj_generation/src/drone_connection.cpp
+++ traj_generation/src/drone_connection.cpp
@@ -9,6 +9,7 @@
 #include <iomanip> // for std::fixed and std::setprecision
 #include <tf/tf.h> // Include the necessary header
 #include <tf/transform_datatypes.h>
+#include "std_msgs/Float64.h"
 
 
 class drone_connection
@@ -29,9 +30,30 @@ class drone_connection
         ros::Publisher pose_pub;
         ros::Publisher error_pub;
         ros::Publisher cmd_vel_unstmpd_pub;
+
+    /**************** PID Stuff: ****************/
+        
+        ros::Publisher x_pos_pid_setp_pub, x_pos_pid_state_pub;
+        ros::Publisher y_pos_pid_setp_pub, y_pos_pid_state_pub;        
+        ros::Publisher z_pos_pid_setp_pub, z_pos_pid_state_pub;  
+
+        ros::Subscriber x_pos_pid_out_sub, y_pos_pid_out_sub, z_pos_pid_out_sub;
+
+        void x_pos_pid_cb(const std_msgs::Float64::ConstPtr& msg);
+        void y_pos_pid_cb(const std_msgs::Float64::ConstPtr& msg);
+        void z_pos_pid_cb(const std_msgs::Float64::ConstPtr& msg);
+
+        std_msgs::Float64 x_pos_setp, x_pos_state;
+        std_msgs::Float64 y_pos_setp, y_pos_state;
+        std_msgs::Float64 z_pos_setp, z_pos_state;
+
+        double x_pos_pid_out, y_pos_pid_out, z_pos_pid_out;
+
+    /**************** PID Stuff: END ****************/
+
+
         ros::ServiceClient arming_client;
         ros::ServiceClient set_mode_client;
-        
         
         ros::ServiceClient set_cmd_vel_frame;
         
@@ -50,20 +72,19 @@ class drone_connection
         geometry_msgs::Pose error_pose, takeoff;
         ros::Time ros_time_last, ros_time_now;
         ros::Duration passed_time;
-
-        double k_x, k_y, k_z;
         
         double time_last, vel_threshold_lin, vel_threshold_ang; 
         int sim_or_real; // 0 = default, 1 = simulation, 2 = real drone
         double vel_calc_x, vel_calc_y, vel_calc_z;
         bool vel_anomalie_detected;
+        int pid_debug_sync_cntr;
 
 
     public:
         geometry_msgs::PoseStamped start_pose;
         bool vel_cmd_received, pose_cmd_received, new_pose_received, use_cntrl;
-        void get_sim_or_real();
         void calc_cntrl_vel();
+        void pub_to_ros_pid();
         void calc_error();
         void send_vel_cmds_to_drone();
         int establish_connection_and_take_off();
@@ -91,9 +112,9 @@ drone_connection::drone_connection(ros::NodeHandle& nh)
     pose_cmd_sub = nh.subscribe<geometry_msgs::PoseStamped>
         ("/pose_cmd_2_drone", 10, &drone_connection::pose_cmd_cb, this); // 19.2 Hz
     pose_sub_sim = nh.subscribe<nav_msgs::Odometry>
-        ("/mavros/odometry/out", 10, &drone_connection::sim_pose_cb,this);  // 19.2 Hz
+        ("/mavros/odometry/out", 10, &drone_connection::sim_pose_cb, this);  // 19.2 Hz
     pose_sub_real = nh.subscribe<geometry_msgs::PoseStamped>
-        ("/optitrack/pose", 10, &drone_connection::real_pose_cb,this);  
+        ("/optitrack/pose", 10, &drone_connection::real_pose_cb, this);  
     
     /* --- Sending CMDS to Drone --- */
     pose_pub = nh.advertise<geometry_msgs::PoseStamped>
@@ -114,6 +135,36 @@ drone_connection::drone_connection(ros::NodeHandle& nh)
     /* --- Other STUFF --- */
     error_pub = nh.advertise<geometry_msgs::Pose>
         ("/error_setpoint_and_real", 10);
+    
+    /* --- ROS PID Controller --- */        
+
+    x_pos_pid_setp_pub = nh.advertise<std_msgs::Float64>
+        ("/x_pos/setpoint", 10);
+    x_pos_pid_state_pub = nh.advertise<std_msgs::Float64>
+        ("/x_pos/state", 10);
+    x_pos_pid_out_sub = nh.subscribe<std_msgs::Float64>
+        ("/x_pos/control_effort", 10, &drone_connection::x_pos_pid_cb, this);
+
+    y_pos_pid_setp_pub = nh.advertise<std_msgs::Float64>
+        ("/y_pos/setpoint", 10);
+    y_pos_pid_state_pub = nh.advertise<std_msgs::Float64>
+        ("/y_pos/state", 10);
+    y_pos_pid_out_sub = nh.subscribe<std_msgs::Float64>
+        ("/y_pos/control_effort", 10, &drone_connection::y_pos_pid_cb, this);
+
+    z_pos_pid_setp_pub = nh.advertise<std_msgs::Float64>
+        ("/z_pos/setpoint", 10);
+    z_pos_pid_state_pub = nh.advertise<std_msgs::Float64>
+        ("/z_pos/state", 10);
+    z_pos_pid_out_sub = nh.subscribe<std_msgs::Float64>
+        ("/z_pos/control_effort", 10, &drone_connection::z_pos_pid_cb, this);
+
+
+    pid_debug_sync_cntr = 0;
+
+    /* --- ROS PID Controller --- */  
+     
+
 
     start_pose.pose.position.x = takeoff.position.x; // -1
     start_pose.pose.position.y = takeoff.position.y; // 2
@@ -131,13 +182,11 @@ drone_connection::drone_connection(ros::NodeHandle& nh)
     pose_cmd_received = false;
     vel_anomalie_detected = false;
     new_pose_received = false;
-    vel_calc_x = 0;
-    vel_calc_y = 0;
-    vel_calc_z = 0;
-    k_x = 0.08;
-    k_y = 0.3;
-    k_z = 0.1;
-    vel_threshold_lin = 0.4;       // 0.4 m/s
+    // vel_calc_x = 0;
+    // vel_calc_y = 0;
+    // vel_calc_z = 0;
+
+    vel_threshold_lin = 0.45;       // 0.4 m/s
     vel_threshold_ang = M_PI / 3 ; //= 1.047 rad per second = 60 degree / s
     sim_or_real = 0;
 
@@ -189,45 +238,70 @@ void drone_connection::vel_cmd_cb(const geometry_msgs::Twist::ConstPtr& msg){
 
 }
 
+void drone_connection::x_pos_pid_cb(const std_msgs::Float64::ConstPtr& msg)
+{
+    std::cout << pid_debug_sync_cntr << " - B\n";
+    x_pos_pid_out = msg->data;
+} 
+
+void drone_connection::y_pos_pid_cb(const std_msgs::Float64::ConstPtr& msg)
+{
+    y_pos_pid_out = msg->data;
+}
+
+void drone_connection::z_pos_pid_cb(const std_msgs::Float64::ConstPtr& msg)
+{
+    z_pos_pid_out = msg->data;
+}
+
+
 /////////////////////////////// CALLBACKS END ///////////////////////////////
 
 
 
+void drone_connection::pub_to_ros_pid(){
+    
+    std::cout << pid_debug_sync_cntr << " - A\n";
 
-/*
-void drone_connection::get_sim_or_real()
-{
-    if (pose_sub_sim.getNumPublishers() > 0 && pose_sub_real.getNumPublishers() > 0) {
-        throw std::runtime_error("Both sim and real pose publishers are active. Only one should be active at a time.");
-    }
+    x_pos_setp.data = pose_cmd_in.pose.position.x; // to transforme it to a std_msgs::Float64
+    x_pos_state.data = curr_pose.pose.position.x;
+    x_pos_pid_setp_pub.publish(x_pos_setp);
+    x_pos_pid_state_pub.publish(x_pos_state);
 
-    if (pose_sub_sim.getNumPublishers() > 0) sim_or_real = 1;
-    else if (pose_sub_real.getNumPublishers() > 0) sim_or_real = 2;
-    else  std::cerr << "No publishers found for either sim or real poses." << std::endl;
+    y_pos_setp.data = pose_cmd_in.pose.position.y; // to transforme it to a std_msgs::Float64
+    y_pos_state.data = curr_pose.pose.position.y;
+    y_pos_pid_setp_pub.publish(y_pos_setp);
+    y_pos_pid_state_pub.publish(y_pos_state);
+
+    z_pos_setp.data = pose_cmd_in.pose.position.z; // to transforme it to a std_msgs::Float64
+    z_pos_state.data = curr_pose.pose.position.z;
+    z_pos_pid_setp_pub.publish(z_pos_setp);
+    z_pos_pid_state_pub.publish(z_pos_state);
     
 }
-*/
+
 
 void drone_connection::calc_cntrl_vel(){
 
+        
         passed_time = ros::Time::now() - ros_time_last; // = 0.052 sec = aprx. 20 hz
+        ros_time_last = ros::Time::now();
 
         // calc k controller values:
-        vel_calc_x = k_x * (error_pose.position.x / passed_time.toSec()); // not working really good...
-        vel_calc_y = k_y * (error_pose.position.y / passed_time.toSec()); // not working really good...
-        vel_calc_z = k_z * (error_pose.position.z / passed_time.toSec());
+        std::cout << pid_debug_sync_cntr << " - C\n";
+        pid_debug_sync_cntr++;
+
+        // vel_calc_x = x_pos_pid_out / passed_time.toSec();
+        // vel_calc_y = y_pos_pid_out / passed_time.toSec();
+        // vel_calc_z = z_pos_pid_out / passed_time.toSec();
 
         //std::cout << "calculated vel.: "<< vel_calc_x << std::endl;
         //std::cout << "real vel.:       "<< vel_cmd_in.linear.z << "\n---\n";
         
-        // get vel. command msg. ready:
-        vel_cmd_send = vel_cmd_in;
-
-        //vel_cmd_send.linear.x = vel_calc_x; 
-        //vel_cmd_send.linear.y = vel_calc_y;
-        vel_cmd_send.linear.z = vel_calc_z; 
+        vel_cmd_send.linear.x = x_pos_pid_out / passed_time.toSec(); // vel_calc_x; 
+        vel_cmd_send.linear.y = y_pos_pid_out / passed_time.toSec(); // vel_calc_y;
+        vel_cmd_send.linear.z = z_pos_pid_out / passed_time.toSec(); // vel_calc_z; 
         
-        ros_time_last = ros::Time::now();
 }
 
  void drone_connection::calc_error() {
@@ -254,20 +328,23 @@ void drone_connection::calc_cntrl_vel(){
     */
 
  }
-
+ 
  void drone_connection::send_vel_cmds_to_drone(){
 
-    bool send_once = false;
-    
-
+  
     std::cout << std::fixed << std::showpoint << std::setprecision(3);
-    if(send_once == false){ // to not overfill the whole command line
 
-        std::cout << "---\nVelocities:\nLin:"   << vel_cmd_in.linear.x << " x; " 
-                                                << vel_cmd_in.linear.y << " y; " 
-                                                << vel_cmd_in.linear.z << " z;\nAng: " // x & y = 0
-                                                << vel_cmd_in.angular.z << " yaw;\n";
-    }
+        std::cout << "---\nVelocities:\nCalc::"   << vel_cmd_send.linear.x << " x; " 
+                                                << vel_cmd_send.linear.y << " y; " 
+                                                << vel_cmd_send.linear.z << " z;\n";//\nAng: ";
+                                                //<< vel_cmd_send.angular.x << " roll; "
+                                                //<< vel_cmd_send.angular.y << " pitch; "
+                                                //<< vel_cmd_send.angular.z << " yaw;\n";
+        std::cout << "Org: "  << vel_cmd_in.linear.x << " x; " 
+                                            << vel_cmd_in.linear.y << " y; " 
+                                            << vel_cmd_in.linear.z << " z;\n---\n";//\nAng: ";
+
+
     
 
     // if the calculatet velocity is under the vel_threshold -> publish it
@@ -286,11 +363,7 @@ void drone_connection::calc_cntrl_vel(){
         vel_cmd_send.angular.z < -vel_threshold_ang || // actually this should be always zero, right?
         vel_anomalie_detected == true)
     {
-
-            if(send_once == false){
-                ROS_ERROR("Velocitie anomalie detected! Sending Velocity ZERO!\n");
-                send_once = true;
-            }
+            ROS_ERROR("Velocitie anomalie detected! Sending Velocity ZERO!\n");
             vel_anomalie_detected = true;
             vel_cmd_send.linear.x = 0.0;
             vel_cmd_send.linear.y = 0.0;
@@ -422,7 +495,7 @@ int main(int argc, char **argv)
         if(drone.use_cntrl == true){ // check if controller should be used
             while(ros::ok()){     
                     
-                drone.calc_error();
+                drone.pub_to_ros_pid();
                 drone.calc_cntrl_vel();
                 drone.send_vel_cmds_to_drone();
 
