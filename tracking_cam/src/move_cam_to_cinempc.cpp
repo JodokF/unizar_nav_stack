8133fc9366ba98413e0b3b7bc848a34b69cf851f
JodokF <caspar.conradi@posteo.org>
2024-06-28 10:12:52 +0200

    tracking cam deviation bug corrected

--- tracking_cam/src/move_cam_to_cinempc.cpp
+++ tracking_cam/src/move_cam_to_cinempc.cpp
@@ -2,6 +2,10 @@
 #include <tf/transform_listener.h>
 #include <geometry_msgs/TransformStamped.h>
 
+#include <ros/ros.h>
+#include <tf/tf.h>
+#include <tf/transform_datatypes.h>
+
 int main(int argc, char** argv) {
   ros::init(argc, argv, "move_cam_to_cinempc");
   ros::NodeHandle nh;
@@ -9,8 +13,8 @@ int main(int argc, char** argv) {
   tf::TransformListener listener;
   tf::StampedTransform transform;
 
-  std::string source_frame = "odom";  // replace with your frame
-  std::string target_frame = "cine_mpc";  // replace with your frame
+  std::string source_frame = "odom";  // calculate the position from here
+  std::string target_frame = "cine_mpc";  // to here
 
   ros::Rate rate(10.0); // Check at 10 Hz
   
@@ -18,13 +22,22 @@ int main(int argc, char** argv) {
     try {
       listener.lookupTransform(source_frame, target_frame, ros::Time(0), transform);
 
+      double qx = transform.getRotation().getX();
+      double qy = transform.getRotation().getY();
+      double qz = transform.getRotation().getZ();
+      double qw = transform.getRotation().getW();
+
+      tf::Quaternion quaternion(qx, qy, qz, qw);
+      double roll, pitch, yaw;
+      tf::Matrix3x3(quaternion).getRPY(roll, pitch, yaw);
+
       // Set parameters based on the transform
       nh.setParam("/frame_offset/x", transform.getOrigin().x());
       nh.setParam("/frame_offset/y", transform.getOrigin().y());
-      nh.setParam("/frame_offset/z", transform.getOrigin().z());
-      nh.setParam("/frame_offset/roll", transform.getRotation().getX());
-      nh.setParam("/frame_offset/pitch", transform.getRotation().getY());
-      nh.setParam("/frame_offset/yaw", transform.getRotation().getZ());
+      nh.setParam("/frame_offset/z", transform.getOrigin().z()); 
+      nh.setParam("/frame_offset/roll", roll);
+      nh.setParam("/frame_offset/pitch", pitch);
+      nh.setParam("/frame_offset/yaw", yaw);
 
       ROS_INFO("Transform set as parameters");
       break; // Exit once parameters are set
