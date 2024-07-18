44417c5b5bb6f6a1644414e30428675d978f68df
JodokF <caspar.conradi@posteo.org>
2024-07-18 13:13:52 +0200

    Sandbox test

--- sandbox/src/sandbox.cpp
+++ sandbox/src/sandbox.cpp
@@ -1,10 +1,6 @@
-
-
 #include <ros/ros.h>
 #include <tf/tf.h>
-
 #include <geometry_msgs/Pose.h>
-
 #include <tf2_ros/transform_listener.h>
 #include <tf2_sensor_msgs/tf2_sensor_msgs.h>
 #include <tf/transform_datatypes.h>
@@ -12,54 +8,61 @@
 #include <nav_msgs/Odometry.h>
 
 
-
-
-class poseTfBroadcaster {
+class tf_intercom {
     private:
 
     ros::NodeHandle nh;
-    ros::Subscriber subs;
-    ros::Publisher pub;
-    geometry_msgs::PoseStamped transformed_pose, temp_pose;
-
-    tf2_ros::Buffer tf_buffer_;
-    tf2_ros::TransformListener tf_listener_; 
+    geometry_msgs::PoseStamped tf_pose_org, tf_pose_new;
+    geometry_msgs::TransformStamped tf_transformation;
+    tf2_ros::Buffer tf_buffer;
+    tf2_ros::TransformListener tf_listener; 
+    ros::Publisher read_tf;
+    std::string target_frame, source_frame;
 
     public:
-        poseTfBroadcaster() :  tf_listener_(tf_buffer_, nh){
+        tf_intercom() :  tf_listener(tf_buffer, nh){
             nh = ros::NodeHandle();
-            subs = nh.subscribe<nav_msgs::Odometry>("/camera/odom/sample_throttled",10,&poseTfBroadcaster::poseCallback,this);
- 	        pub = nh.advertise<geometry_msgs::PoseStamped>("/walawalabumbum",10);
+            
+            read_tf = nh.advertise<geometry_msgs::PoseStamped>("/tf_read_pose",10);
+            target_frame = "odom";
+            source_frame = "base_link";
+
+            // this is necessary otherwise the orientation will be n.a.n.:
+            tf_pose_org.pose.orientation.w = 1.0;
         }
 
-        void poseCallback(nav_msgs::Odometry recieved_pose){
-
-            temp_pose.header = recieved_pose.header;
-            temp_pose.pose = recieved_pose.pose.pose;
-
-            geometry_msgs::TransformStamped tf_odom_odomNED;
-            try{
-                tf_odom_odomNED = tf_buffer_.lookupTransform("odom", "camera_odom_frame", ros::Time(0));
-            } catch (tf2::TransformException ex){
-                ROS_ERROR("%s",ex.what());
-            }
-	    tf2::doTransform(temp_pose, transformed_pose, tf_odom_odomNED);
-	    transformed_pose.header.frame_id="odom";
-            transformed_pose.header.stamp=temp_pose.header.stamp;
-        }
-
-	void timedPubCallback(void){
-	    pub.publish(transformed_pose);  
-	}
+        void readtfTree();
 };
+        
 
+void tf_intercom::readtfTree()
+{
+    // to evade some error msgs at the startup
+    if (tf_buffer.canTransform(target_frame, source_frame, ros::Time(0))) {
+        try{
+            tf_transformation = tf_buffer.lookupTransform(target_frame, source_frame, ros::Time(0));
+        } catch (tf2::TransformException ex){
+            ROS_ERROR("%s",ex.what());
+        }
+        
+        tf2::doTransform(tf_pose_org, tf_pose_new, tf_transformation);
+        read_tf.publish(tf_pose_new);
+
+    } 
+    else {
+    ROS_WARN("Transform not available yet, waiting...");
+    ros::Duration(1).sleep();
+    }
+
+}
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "pose_tf_broadcaster");
-    poseTfBroadcaster pose_tf_broadcaster;
+    ros::init(argc, argv, "tf_intercom");
+    tf_intercom tf_intercom;
     ros::Rate loop_rate(40);
+
     while (ros::ok()){
-      pose_tf_broadcaster.timedPubCallback();
+      tf_intercom.readtfTree();
       ros::spinOnce();
       loop_rate.sleep();
     }
